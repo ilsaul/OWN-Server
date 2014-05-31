290d809fc4ad881e9c5319bef7a7f37d03af1f25
Moreno Cattaneo <moreno.cattaneo@gmail.com>
2014-05-31 17:02:38 +0200

    Change from Ant to Gradle to prepare the project

--- OWNServer/src/main/java/org/programmatori/domotica/own/server/TcpIpServer.java
+++ OWNServer/src/main/java/org/programmatori/domotica/own/server/TcpIpServer.java
@@ -1,21 +1,21 @@
 /*
- * OWN Server is 
+ * OWN Server is
  * Copyright (C) 2010-2012 Moreno Cattaneo <moreno.cattaneo@gmail.com>
- * 
+ *
  * This file is part of OWN Server.
- * 
+ *
  * OWN Server is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
+ * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  *  License, or (at your option) any later version.
- * 
+ *
  * OWN Server is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
- * License along with OWN Server.  If not, see 
+ * License along with OWN Server.  If not, see
  * <http://www.gnu.org/licenses/>.
  */
 package org.programmatori.domotica.own.server;
@@ -29,9 +29,9 @@ import org.programmatori.domotica.own.sdk.config.Config;
 import org.programmatori.domotica.own.sdk.server.engine.EngineManager;
 
 /**
- * This class wait the network connection. It's 
- * designed to received Tcp/Ip connections and send them to 
- * {@link ClientConnection} for the management. 
+ * This class wait the network connection. It's
+ * designed to received Tcp/Ip connections and send them to
+ * {@link ClientConnection} for the management.
  *
  * @author Moreno Cattaneo (moreno.cattaneo@gmail.com)
  * @version 1.2.1, 16/10/2010
@@ -53,7 +53,7 @@ public class TcpIpServer implements Runnable {
 		Config.getInstance().addThread(Thread.currentThread());
 
 		this.engine = engine;
-		
+
 		list = new ClientList();
 
 		int port = Config.getInstance().getServerPort();
@@ -76,12 +76,12 @@ public class TcpIpServer implements Runnable {
 		while (!Config.getInstance().isExit()) {
 			try {
 				if (list.getSize() < maxConnections) {
-					log.info("Client Connecting ... (N°" + list.getSize() + ")");
-					
+					log.info("Client Connecting ... (NÂº" + list.getSize() + ")");
+
 					// Connection respond
 					ClientConnection connection = new ClientConnection(serverSocket.accept(), this, engine);
 					int size = list.add(connection);
-					
+
 					String name = "Conn #" + connection.getId();
 					log.debug("Connection " + size + ": '" + name + "'");
 					Thread t = new Thread(connection, name);
@@ -89,7 +89,7 @@ public class TcpIpServer implements Runnable {
 				} else {
 					log.warn("Maximum number of connection reached " + maxConnections);
 				}
-			
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -97,7 +97,7 @@ public class TcpIpServer implements Runnable {
 
 		log.info("Server End");
 	}
-	
+
 	public boolean isRunning() {
 		return Thread.currentThread().getState() != Thread.State.TERMINATED;
 	}
