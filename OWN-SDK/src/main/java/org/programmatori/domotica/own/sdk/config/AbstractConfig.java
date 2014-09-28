9d8fa78bb2a849ff83e5c7763aa0560a58bb31d3
Saul <ilsaul2@gmail.com>
2014-09-29 00:14:26 +0200

    start introducing slf4j

--- OWN-SDK/src/main/java/org/programmatori/domotica/own/sdk/config/AbstractConfig.java
+++ OWN-SDK/src/main/java/org/programmatori/domotica/own/sdk/config/AbstractConfig.java
@@ -1,21 +1,21 @@
 /*
- * OWN Server is 
+ * OWN Server is
  * Copyright (C) 2010-2012 Moreno Cattaneo <moreno.cattaneo@gmail.com>
- * 
+ *
  * This file is part of OWN Server.
- * 
+ *
  * OWN Server is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
+ * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  *  License, or (at your option) any later version.
- * 
+ *
  * OWN Server is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
- * License along with OWN Server.  If not, see 
+ * License along with OWN Server.  If not, see
  * <http://www.gnu.org/licenses/>.
  */
 package org.programmatori.domotica.own.sdk.config;
@@ -28,15 +28,19 @@ import org.apache.commons.configuration.XMLConfiguration;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.programmatori.domotica.own.sdk.utils.LogUtility;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Abstract configuration
- * 
+ *
  * @version 2.1, 16/10/2010
  * @author Moreno Cattaneo (moreno.cattaneo@gmail.com)
  */
 public abstract class AbstractConfig {
-	private static Log log = LogFactory.getLog(AbstractConfig.class);
+	//private static Log log = LogFactory.getLog(AbstractConfig.class);
+	private static Logger logger = LoggerFactory.getLogger(AbstractConfig.class);
+
 	public static final String DEFAULT_CONFIG_FOLDER = "conf";
 	public static final String DEFAULT_CONFIG_PATH = "./" + DEFAULT_CONFIG_FOLDER;
 	public static String HOME_FILE = "home.config";
@@ -62,7 +66,7 @@ public abstract class AbstractConfig {
 		try {
 			config = new XMLConfiguration(getConfigPath() + "/config.xml");
 			config.setAutoSave(true);
-			log.info("Config File: " + config.getURL());
+			logger.info("Config File: {}", config.getURL());
 
 			configLoaded = true;
 		} catch (ConfigurationException e) {
@@ -109,7 +113,7 @@ public abstract class AbstractConfig {
 
 		if (pos > -1) {
 			config.setProperty(nodeToSearch + "(" + pos + ")." + param, value);
-			log.debug("Param: " + nodeToSearch + "(" + pos + ")." + param + " = " + value);
+			logger.debug("Param: {} ({}).{}={}", nodeToSearch, pos, param, value);
 		}
 	}
 
@@ -119,7 +123,7 @@ public abstract class AbstractConfig {
 
 		if (pos > -1) {
 			value = (String) config.getProperty(nodeToSearch + "(" + pos + ")." + param);
-			log.debug("Param: " + nodeToSearch + "(" + pos + ")." + param + " = " + value);
+			logger.debug("Param: {} ({}).{}={}", nodeToSearch, pos, param, value);
 		}
 
 		return value;
@@ -141,7 +145,7 @@ public abstract class AbstractConfig {
 
 	public String getConfigPath() {
 		String path = configPath;
-		
+
 		if (path == null) {
 			try {
 				//String home = getOldHomeDirectory(HOME_FILE); // Deprecated System
@@ -149,7 +153,8 @@ public abstract class AbstractConfig {
 				path = home + "/" + DEFAULT_CONFIG_FOLDER;
 
 			} catch (Exception e) {
-				log.error(LogUtility.getErrorTrace(e));
+				//log.error(LogUtility.getErrorTrace(e));
+				logger.error("Error", LogUtility.getErrorTrace(e));
 				path = DEFAULT_CONFIG_PATH;
 			}
 		}
@@ -237,7 +242,7 @@ public abstract class AbstractConfig {
 
 		return home;
 	}
-	
+
 	/**
 	 * Give the home of the project. <br>
 	 * For return the home of the project need to have a file in the home
@@ -246,29 +251,29 @@ public abstract class AbstractConfig {
 	 */
 	public static String getHomeDirectory() {
 		File f = new File("");
-		
+
 		String filePath =  f.getAbsolutePath();
-		
-		
+
+
 		return getPathFromString(filePath);
 	}
-		
+
 		private static String getPathFromString(String path) {
 			Log log = LogFactory.getLog(AbstractConfig.class);
-			
+
 			String separator = File.separator;
 			boolean first = true;
 			String home = "";
-			
+
 			log.debug("Path " + path);
-			
+
 			StringTokenizer st = new StringTokenizer(path, "/"); // URI Separator
 			//st.nextToken(); // don't calculate "file:"
 			while (st.hasMoreTokens()) {
 				String folder = st.nextToken();
 				log.debug("Foledr: " + folder);
-				
-				boolean bFile = !(folder.equals("file:")); 
+
+				boolean bFile = !(folder.equals("file:"));
 
 				// BUG Linux starting slash
 				if (separator.equals("/") && first) {
@@ -294,7 +299,7 @@ public abstract class AbstractConfig {
 					log.debug("home: " + home);
 				}
 			}
-			
+
 			return home;
 		}
 
